d1965e35f486c363d2ee71879b9bcb04be6a2975
ian <ian_arsenault@protonmail.com>
2017-11-04 16:21:07 -0400

    add events for cards

--- PTClinic/PTClinic/Admin.Designer.cs
+++ PTClinic/PTClinic/Admin.Designer.cs
@@ -30,19 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label7 = new System.Windows.Forms.Label();
             this.btnLogOut = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbBtnVisit = new System.Windows.Forms.PictureBox();
+            this.pbBtnNewPatient = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBtnVisit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBtnNewPatient)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -56,17 +56,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(784, 73);
             this.panel1.TabIndex = 16;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(79, 20);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 29);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Home";
             // 
             // btnLogOut
             // 
@@ -82,6 +71,26 @@
             this.btnLogOut.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnLogOut.UseVisualStyleBackColor = false;
             this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 40);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(79, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 29);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Home";
             // 
             // label1
             // 
@@ -112,32 +121,29 @@
             this.panel2.Size = new System.Drawing.Size(313, 51);
             this.panel2.TabIndex = 19;
             // 
-            // pictureBox3
+            // pbBtnVisit
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(401, 136);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(322, 286);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox3.TabIndex = 21;
-            this.pictureBox3.TabStop = false;
+            this.pbBtnVisit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbBtnVisit.Image = ((System.Drawing.Image)(resources.GetObject("pbBtnVisit.Image")));
+            this.pbBtnVisit.Location = new System.Drawing.Point(401, 136);
+            this.pbBtnVisit.Name = "pbBtnVisit";
+            this.pbBtnVisit.Size = new System.Drawing.Size(322, 286);
+            this.pbBtnVisit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbBtnVisit.TabIndex = 21;
+            this.pbBtnVisit.TabStop = false;
+            this.pbBtnVisit.Click += new System.EventHandler(this.pbBtnVisit_Click);
             // 
-            // pictureBox2
+            // pbBtnNewPatient
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(41, 136);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(323, 290);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox2.TabIndex = 20;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(29, 14);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(44, 40);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
+            this.pbBtnNewPatient.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbBtnNewPatient.Image = ((System.Drawing.Image)(resources.GetObject("pbBtnNewPatient.Image")));
+            this.pbBtnNewPatient.Location = new System.Drawing.Point(41, 136);
+            this.pbBtnNewPatient.Name = "pbBtnNewPatient";
+            this.pbBtnNewPatient.Size = new System.Drawing.Size(321, 289);
+            this.pbBtnNewPatient.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbBtnNewPatient.TabIndex = 20;
+            this.pbBtnNewPatient.TabStop = false;
+            this.pbBtnNewPatient.Click += new System.EventHandler(this.pbBtnNewPatient_Click);
             // 
             // Admin
             // 
@@ -145,19 +151,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 465);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pbBtnVisit);
+            this.Controls.Add(this.pbBtnNewPatient);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Admin";
             this.Text = "Administrator";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBtnVisit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBtnNewPatient)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,7 +178,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pbBtnNewPatient;
+        private System.Windows.Forms.PictureBox pbBtnVisit;
     }
 }
--- PTClinic/PTClinic/Admin.cs
+++ PTClinic/PTClinic/Admin.cs
@@ -22,6 +22,12 @@ namespace PTClinic
             Login.Hide();
 
             setButtonIcon();
+
+            // Mouse Enter events for cards
+            this.pbBtnNewPatient.MouseEnter += new EventHandler(pbBtnNewPatient_MouseEnter);
+            this.pbBtnVisit.MouseEnter += new EventHandler(pbBtnVisit_MouseEnter);
+            this.pbBtnNewPatient.MouseLeave += new EventHandler(pbBtnNewPatient_MouseLeave);
+            this.pbBtnVisit.MouseLeave += new EventHandler(pbBtnVisit_MouseLeave);
         }
 
         private void setButtonIcon()
@@ -34,6 +40,44 @@ namespace PTClinic
         private void btnLogOut_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void  pbBtnNewPatient_Click(object sender, EventArgs e)
+        {
+            // ON CLICK GO TO NEW PATIENT FORM
+
+        }
+
+        private void pbBtnVisit_Click(object sender, EventArgs e)
+        {
+            // ON CLICK GO TO PATIENT VISIT FORM
+        }
+
+
+        private void pbBtnNewPatient_MouseEnter(object sender, EventArgs e)
+        {
+            // ADD BORDER
+            pbBtnNewPatient.BorderStyle = BorderStyle.FixedSingle;
+
+        }
+
+        private void pbBtnVisit_MouseEnter(object sender, EventArgs e)
+        {
+            // ADD BORDER
+            pbBtnVisit.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void pbBtnNewPatient_MouseLeave(object sender, EventArgs e)
+        {
+            // REMOVE BORDER
+            pbBtnNewPatient.BorderStyle = BorderStyle.None;
+
+        }
+
+        private void pbBtnVisit_MouseLeave(object sender, EventArgs e)
+        {
+            // REMOVE BORDER
+            pbBtnVisit.BorderStyle = BorderStyle.None;
         }
     }
 }

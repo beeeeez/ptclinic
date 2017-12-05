ed8d6d98a1ff033e2619926472ef89d8a730f879
OverlordZoltron <jmarsenault@email.neit.edu>
2017-12-05 02:32:55 -0500

    Trying to get PGF to show

--- PTClinic/PTClinic/PatientGoalsForm.Designer.cs
+++ PTClinic/PTClinic/PatientGoalsForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PatientGoalsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 456);
+            this.Name = "PatientGoalsForm";
             this.Text = "PatientGoalsForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

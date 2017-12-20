fba788f78a769dc835a1ab3d091ac58852d522e8
OverlordZoltron <jmarsenault@email.neit.edu>
2017-12-19 20:49:16 -0500

    Testing scroll bars.. more information in the Issues section under Autoscroll scrollbar issue

--- PTClinic/PTClinic/AppointmentForm.Designer.cs
+++ PTClinic/PTClinic/AppointmentForm.Designer.cs
@@ -417,6 +417,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1267, 629);
             this.Controls.Add(this.lblFeedback);
